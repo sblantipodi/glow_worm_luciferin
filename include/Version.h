3e303900467ff07366aa237dcc6040a5d8d6af2c
Davide Perini <perini.davide@dpsoftware.org>
2020-08-05 18:28:42 +0200

    CMake erase FS, lib update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "3.8.2"
+        #define VERSION "3.8.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-08-02 00:13:26.098528"
+        #define BUILD_TIMESTAMP "2020-08-04 17:58:10.276173"
     #endif
     
