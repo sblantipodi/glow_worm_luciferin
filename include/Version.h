fd7aa09c0457545b74ebd96f74cd783df8ba1982
Davide Perini <perini.davide@dpsoftware.org>
2020-08-13 16:30:38 +0200

    MQTT is now able to drive the bias lighting effect without the needs of the serial port. Faster output stream.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.0.2"
+        #define VERSION "4.0.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-08-11 19:29:46.214981"
+        #define BUILD_TIMESTAMP "2020-08-11 21:07:47.530989"
     #endif
     
