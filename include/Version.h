ab3a9261270584e31f583a1472664f45901b1129
Davide Perini <perini.davide@dpsoftware.org>
2020-08-19 02:39:33 +0200

    esp32

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.0.6"
+        #define VERSION "4.0.5"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-08-18 14:52:14.248962"
+        #define BUILD_TIMESTAMP "2020-08-19 02:35:45.456686"
     #endif
     
