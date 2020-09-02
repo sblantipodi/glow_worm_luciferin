d4849301b7860eac16c2aba6cdfaed4d7d4b68d5
Davide Perini <perini.davide@dpsoftware.org>
2020-09-02 16:12:00 +0200

    firmware update

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.1.1"
+        #define VERSION "4.1.4"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2020-08-31 11:18:59.360889"
