64092cda349e6d60cf1b1395bd45ff97cb01e93b
Davide Perini <perini.davide@dpsoftware.org>
2020-09-12 02:01:05 +0200

    set brightness during ambient light effect fix LEDs remains ON when stopping ambint light effect

--- include/GlowWormLuciferin.h
+++ include/GlowWormLuciferin.h
@@ -3,19 +3,19 @@
   All in one Bias Lighting system for PC
   
   Copyright (C) 2020  Davide Perini
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy of 
-  this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-  copies of the Software, and to permit persons to whom the Software is 
-  furnished to do so, subject to the following conditions:
 
-  The above copyright notice and this permission notice shall be included in 
-  all copies or substantial portions of the Software.
-  
-  You should have received a copy of the MIT License along with this program.  
-  If not, see <https://opensource.org/licenses/MIT/>.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
   * Components:
    - Arduino C++ sketch running on an ESP8266EX D1 Mini from Lolin running @ 160MHz
--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.1.4"
+        #define VERSION "4.1.8"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-08-31 11:18:59.360889"
+        #define BUILD_TIMESTAMP "2020-09-12 01:57:57.931853"
     #endif
     
