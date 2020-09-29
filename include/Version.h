eeb40667040fef76ab617923b9c2e2305e9b30f4
Davide Perini <perini.davide@dpsoftware.org>
2020-09-29 21:51:14 +0200

    improved checksum

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.2.2"
+        #define VERSION "4.2.3"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2020-09-22 21:07:09.700902"
