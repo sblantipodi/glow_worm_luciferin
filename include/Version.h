64d7ac67527f300152be20bc96482a1c81731519
Davide Perini <perini.davide@dpsoftware.org>
2020-12-20 14:33:29 +0100

    home assistant framerate sensor

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.2.76"
+        #define VERSION "4.2.79"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-12-19 18:20:16.242170"
+        #define BUILD_TIMESTAMP "2020-12-20 03:19:56.511868"
     #endif
     
