8e4b9bb86b81d100a3b598fed34304c01dd4d9d1
Davide Perini <perini.davide@dpsoftware.org>
2020-12-22 14:13:41 +0100

    prebuild boards support

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.2.86"
+        #define VERSION "4.3.0"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-12-20 23:47:24.806259"
+        #define BUILD_TIMESTAMP "2020-12-22 14:05:12.907039"
     #endif
     
