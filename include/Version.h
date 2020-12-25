48b1e51a4efa60cf45190613b62229fe38d2d139
Davide Perini <perini.davide@dpsoftware.org>
2020-12-25 01:11:13 +0100

    improved latency

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.3.2"
+        #define VERSION "4.3.6"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2020-12-24 23:20:56.716175"
