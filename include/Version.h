2e928770ed29d47516434dc02f8323ac2141158c
Davide Perini <perini.davide@dpsoftware.org>
2021-01-20 17:51:48 +0100

    hotfix release

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.6.2"
+        #define VERSION "4.6.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-01-20 12:36:35.737162"
+        #define BUILD_TIMESTAMP "2021-01-20 17:50:10.673386"
     #endif
     
