e93c00602f3d8c9721e62af774117cf4811f2194
Davide Perini <perini.davide@dpsoftware.org>
2021-01-20 12:38:41 +0100

    Release v4.6.2

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.2.103"
+        #define VERSION "4.6.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-01-16 18:35:32.154228"
+        #define BUILD_TIMESTAMP "2021-01-20 12:36:35.737162"
     #endif
     
