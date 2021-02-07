84b105a55ca2c2c3e959d996dfd532e9d938c9e0
Davide Perini <perini.davide@dpsoftware.org>
2021-02-07 16:07:27 +0100

    Release v4.7.3

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.6.24"
+        #define VERSION "4.7.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-01-26 15:40:52.571800"
+        #define BUILD_TIMESTAMP "2021-02-07 16:06:27.846317"
     #endif
     
