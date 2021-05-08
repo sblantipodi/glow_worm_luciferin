7c790dab1affeaf3abde776be1aef9a6f14c21a8
Davide Perini <perini.davide@dpsoftware.org>
2021-05-08 15:56:52 +0200

    Color correction (white temperature)

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.7.142"
+        #define VERSION "4.8.5"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-05-07 14:14:19.945174"
+        #define BUILD_TIMESTAMP "2021-05-08 10:53:39.038896"
     #endif
     
