83baba29270ad78f2ca533e4628adff16f400b99
Davide Perini <perini.davide@dpsoftware.org>
2021-05-31 21:11:41 +0200

    Effects, audio loopback, night mode #45

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "4.9.8"
+        #define VERSION "4.9.9"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2021-05-31 00:09:23.524172"
+        #define BUILD_TIMESTAMP "2021-05-31 21:09:50.885706"
     #endif
     
