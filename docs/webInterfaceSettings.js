8f6f53ea51d41661739feb2fbf978a3f4f3383cd
Davide Perini <perini.davide@dpsoftware.org>
2021-11-08 11:41:56 +0100

    Update webInterfaceSettings.js

--- docs/webInterfaceSettings.js
+++ docs/webInterfaceSettings.js
@@ -1,27 +1,30 @@
 function poll() {
     var sleep = time => new Promise(resolve => setTimeout(resolve, time))
     var poll = (promiseFn, time) => promiseFn().then(sleep(time).then(() => poll(promiseFn, time)))
+    var received = false;
     poll(() => new Promise(() => {
-      const http = new XMLHttpRequest();
-      http.open('GET', 'getsettings');
-      http.send();
-      http.onload = () => {
-        console.log(http.responseText);
-        var prefs = JSON.parse(http.responseText);
-        console.log(prefs);
-          $('#deviceName').val(prefs.deviceName);
-          $('#microcontrollerIP').val(prefs.ip);
-          if (prefs.mqttIp > 0) {
-            $("#mqttCheckbox").prop('checked', true);
-          }
-          $('#deviceName').val(prefs.deviceName);
-          $('#inputMqttIp').val(prefs.mqttIp);
-          $('#mqttPort').val(prefs.mqttPort);
-          $('#mqttuser').val(prefs.mqttuser);
-          $('#mqttpass').val(prefs.mqttpass);
-          $('#additionalParam').val(prefs.gpio);
-
-      }
+        if (!received) {
+            received = true;
+            const http = new XMLHttpRequest();
+            http.open('GET', 'getsettings');
+            http.send();
+            http.onload = () => {
+                console.log(http.responseText);
+                var prefs = JSON.parse(http.responseText);
+                console.log(prefs);
+                $('#deviceName').val(prefs.deviceName);
+                $('#microcontrollerIP').val(prefs.ip);
+                if (prefs.mqttIp > 0) {
+                    $("#mqttCheckbox").prop('checked', true);
+                }
+                $('#deviceName').val(prefs.deviceName);
+                $('#inputMqttIp').val(prefs.mqttIp);
+                $('#mqttPort').val(prefs.mqttPort);
+                $('#mqttuser').val(prefs.mqttuser);
+                $('#mqttpass').val(prefs.mqttpass);
+                $('#additionalParam').val(prefs.gpio);
+            }
+        }
     }), 5000);
 }
 function callDevice(payload) {
