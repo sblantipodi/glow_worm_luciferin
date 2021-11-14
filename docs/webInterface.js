ea2dffa03ab27aa540514f8d4964a94235fa3e41
Davide Perini <perini.davide@dpsoftware.org>
2021-11-14 18:29:59 +0100

    Update webInterface.js

--- docs/webInterface.js
+++ docs/webInterface.js
@@ -73,7 +73,7 @@ colorPicker.on(['input:end'], function(color) {
 const sleep = (s) => {
   return new Promise(resolve => setTimeout(resolve, (s)));
 };
-sleep(200).then(() => {
+sleep(100).then(() => {
   $('#effectSelect').append(new Option("Solid", "solid"));
   $('#effectSelect').append(new Option("Fire", "fire"));
   $('#effectSelect').append(new Option("Twinkle", "twinkle"));
