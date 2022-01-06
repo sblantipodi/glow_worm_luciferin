35091242ffb81940b95107d3da06bb17c1abed45
Davide Perini <perini.davide@dpsoftware.org>
2022-01-06 21:49:56 +0100

    GPL License update

--- include/GlowWormLuciferin.h
+++ include/GlowWormLuciferin.h
@@ -2,7 +2,7 @@
   GlowWormLuciferin.h - Glow Worm Luciferin for Firefly Luciferin
   All in one Bias Lighting system for PC
 
-  Copyright (C) 2020 - 2021  Davide Perini
+  Copyright (C) 2020 - 2022  Davide Perini
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
