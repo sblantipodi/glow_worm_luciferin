d47165188a54829d6b2662a55a10789a2a7957f8
Davide Perini <perini.davide@dpsoftware.org>
2022-03-16 22:11:43 +0100

    RGBW support

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.4.12"
+        #define VERSION "5.4.9"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2022-03-16 20:47:59.293984"
