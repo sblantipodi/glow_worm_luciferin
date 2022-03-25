4fe76d6cc8b82beee827effa7fcb04d0587a0ce3
Davide Perini <perini.davide@dpsoftware.org>
2022-03-25 17:38:22 +0100

    Arduino Bootstrapper (v1.12.9)

--- src/GlowWormLuciferin.cpp
+++ src/GlowWormLuciferin.cpp
@@ -27,120 +27,119 @@
 void setup() {
 
   firmwareVersion = VERSION;
-//  // if fastDisconnectionManagement we need to execute the disconnection callback immediately
-//  fastDisconnectionManagement = true;
-//  // BaudRate from configuration storage
-//#if defined(ESP32)
-//  String baudRateFromStorage = bootstrapManager.readValueFromFile(BAUDRATE_FILENAME, BAUDRATE_PARAM, false);
-//#else
-//  String baudRateFromStorage = bootstrapManager.readValueFromFile(BAUDRATE_FILENAME, BAUDRATE_PARAM);
-//#endif
-//  if (!baudRateFromStorage.isEmpty() && baudRateFromStorage != ERROR && baudRateFromStorage.toInt() != 0) {
-//    baudRateInUse = baudRateFromStorage.toInt();
-//  }
-//  int baudRateToUse = globals.setBaudRateInUse(baudRateInUse);
-//  Serial.begin(baudRateToUse);
-  Serial.begin(115200);
+  // if fastDisconnectionManagement we need to execute the disconnection callback immediately
+  fastDisconnectionManagement = true;
+  // BaudRate from configuration storage
+#if defined(ESP32)
+  String baudRateFromStorage = bootstrapManager.readValueFromFile(BAUDRATE_FILENAME, BAUDRATE_PARAM, false);
+#else
+  String baudRateFromStorage = bootstrapManager.readValueFromFile(BAUDRATE_FILENAME, BAUDRATE_PARAM);
+#endif
+  if (!baudRateFromStorage.isEmpty() && baudRateFromStorage != ERROR && baudRateFromStorage.toInt() != 0) {
+    baudRateInUse = baudRateFromStorage.toInt();
+  }
+  int baudRateToUse = globals.setBaudRateInUse(baudRateInUse);
+  Serial.begin(baudRateToUse);
 //  while (!Serial); // wait for serial attach
-//  Serial.print(F("BAUDRATE IN USE="));
-//  Serial.println(baudRateToUse);
+  Serial.print(F("BAUDRATE IN USE="));
+  Serial.println(baudRateToUse);
   if (Serial.available() > 0 && Serial.peek() == 'I') bootstrapManager.manageImprovWifi();
 
-//  // LED number from configuration storage
-//  String ledNumToUse = bootstrapManager.readValueFromFile(LED_NUM_FILENAME, LED_NUM_PARAM);
-//  if (!ledNumToUse.isEmpty() && ledNumToUse != ERROR && ledNumToUse.toInt() != 0) {
-//    ledManager.dynamicLedNum = ledNumToUse.toInt();
-//  } else {
-//    ledManager.dynamicLedNum = 100;
-//  }
-//  Serial.print(F("\nUsing LEDs="));
-//  Serial.println(ledManager.dynamicLedNum);
-//
-//  // White temp to use
-//  String whiteTempToUse = bootstrapManager.readValueFromFile(WHITE_TEMP_FILENAME, WHITE_TEMP_PARAM);
-//  if (!whiteTempToUse.isEmpty() && whiteTempToUse != ERROR && whiteTempToUse.toInt() != 0) {
-//    whiteTempInUse = whiteTempToUse.toInt();
-//  }
-//  Serial.print(F("\nUsing White temp="));
-//  Serial.println(whiteTempToUse);
-//
-//#ifdef TARGET_GLOWWORMLUCIFERINLIGHT
-//  MAC = WiFi.macAddress();
-//#endif
-//
-//#ifdef TARGET_GLOWWORMLUCIFERINFULL
-//  // LED number from configuration storage
-//  String topicToUse = bootstrapManager.readValueFromFile(networkManager.TOPIC_FILENAME, networkManager.MQTT_PARAM);
-//  if (topicToUse != "null" && !topicToUse.isEmpty() && topicToUse != ERROR && topicToUse != networkManager.topicInUse) {
-//    networkManager.topicInUse = topicToUse;
-//    networkManager.executeMqttSwap(networkManager.topicInUse);
-//  }
-//  Serial.print(F("\nMQTT topic in use="));
-//  Serial.println(networkManager.topicInUse);
-//
-//  // Bootsrap setup() with Wifi and MQTT functions
-//  bootstrapManager.bootstrapSetup(NetworkManager::manageDisconnections, NetworkManager::manageHardwareButton, NetworkManager::callback);
-//#endif
-//
-//  // GPIO pin from configuration storage, overwrite the one saved during initial Arduino Bootstrapper config
-//  String gpioFromStorage = bootstrapManager.readValueFromFile(GPIO_FILENAME, GPIO_PARAM);
-//  int gpioToUse = 0;
-//  if (!gpioFromStorage.isEmpty() && gpioFromStorage != ERROR && gpioFromStorage.toInt() != 0) {
-//    gpioToUse = gpioFromStorage.toInt();
-//  }
-//  if (gpioToUse == 0) {
-//    if (!additionalParam.isEmpty()) {
-//      gpioToUse = additionalParam.toInt();
-//    }
-//  }
-//  switch (gpioToUse) {
-//    case 5: gpioInUse = 5; break;
-//    case 3: gpioInUse = 3; break;
-//    case 16: gpioInUse = 16; break;
-//    default: gpioInUse = 2; break;
-//  }
-//  Serial.print(F("GPIO IN USE="));
-//  Serial.println(gpioInUse);
-//
-//
-//  // Color mode from configuration storage
-//  String colorModeFromStorage = bootstrapManager.readValueFromFile(ledManager.COLOR_MODE_FILENAME, ledManager.COLOR_MODE_PARAM);
-//  if (!colorModeFromStorage.isEmpty() && colorModeFromStorage != ERROR && colorModeFromStorage.toInt() != 0) {
-//    colorMode = colorModeFromStorage.toInt();
-//  }
-//  Serial.print(F("COLOR_MODE IN USE="));
-//  Serial.println(colorMode);
-//  ledManager.initLeds();
-//
-//#if defined(ESP8266)
-//  pinMode(RELAY_PIN, OUTPUT);
-//  digitalWrite(RELAY_PIN, LOW);
-//#elif defined(ESP32)
-//  pinMode(RELAY_PIN_DIG, OUTPUT);
-//  digitalWrite(RELAY_PIN_DIG, LOW);
-//  pinMode(RELAY_PIN_PICO, OUTPUT);
-//  digitalWrite(RELAY_PIN_PICO, LOW);
-//#endif
-//
-//#if defined(ESP32)
-//  esp_task_wdt_init(3000, true); //enable panic so ESP32 restarts
-//  esp_task_wdt_add(NULL); //add current thread to WDT watch
-//#endif
-//#ifdef TARGET_GLOWWORMLUCIFERINFULL
-//  // Begin listening to UDP port
-//  networkManager.UDP.begin(UDP_PORT);
-//  networkManager.broadcastUDP.begin(UDP_BROADCAST_PORT);
-//  Serial.print("Listening on UDP port ");
-//  Serial.println(UDP_PORT);
-//  networkManager.fpsData.reserve(200);
-//  networkManager.prefsData.reserve(200);
-//  networkManager.listenOnHttpGet();
-//#if defined(ESP8266)
-//  // Hey gateway, GlowWorm is here
-//  delay(DELAY_500);
-//  pingESP.ping(WiFi.gatewayIP());
-//#endif
-//#endif
+  // LED number from configuration storage
+  String ledNumToUse = bootstrapManager.readValueFromFile(LED_NUM_FILENAME, LED_NUM_PARAM);
+  if (!ledNumToUse.isEmpty() && ledNumToUse != ERROR && ledNumToUse.toInt() != 0) {
+    ledManager.dynamicLedNum = ledNumToUse.toInt();
+  } else {
+    ledManager.dynamicLedNum = 100;
+  }
+  Serial.print(F("\nUsing LEDs="));
+  Serial.println(ledManager.dynamicLedNum);
+
+  // White temp to use
+  String whiteTempToUse = bootstrapManager.readValueFromFile(WHITE_TEMP_FILENAME, WHITE_TEMP_PARAM);
+  if (!whiteTempToUse.isEmpty() && whiteTempToUse != ERROR && whiteTempToUse.toInt() != 0) {
+    whiteTempInUse = whiteTempToUse.toInt();
+  }
+  Serial.print(F("\nUsing White temp="));
+  Serial.println(whiteTempToUse);
+
+#ifdef TARGET_GLOWWORMLUCIFERINLIGHT
+  MAC = WiFi.macAddress();
+#endif
+
+#ifdef TARGET_GLOWWORMLUCIFERINFULL
+  // LED number from configuration storage
+  String topicToUse = bootstrapManager.readValueFromFile(networkManager.TOPIC_FILENAME, networkManager.MQTT_PARAM);
+  if (topicToUse != "null" && !topicToUse.isEmpty() && topicToUse != ERROR && topicToUse != networkManager.topicInUse) {
+    networkManager.topicInUse = topicToUse;
+    networkManager.executeMqttSwap(networkManager.topicInUse);
+  }
+  Serial.print(F("\nMQTT topic in use="));
+  Serial.println(networkManager.topicInUse);
+
+  // Bootsrap setup() with Wifi and MQTT functions
+  bootstrapManager.bootstrapSetup(NetworkManager::manageDisconnections, NetworkManager::manageHardwareButton, NetworkManager::callback);
+#endif
+
+  // GPIO pin from configuration storage, overwrite the one saved during initial Arduino Bootstrapper config
+  String gpioFromStorage = bootstrapManager.readValueFromFile(GPIO_FILENAME, GPIO_PARAM);
+  int gpioToUse = 0;
+  if (!gpioFromStorage.isEmpty() && gpioFromStorage != ERROR && gpioFromStorage.toInt() != 0) {
+    gpioToUse = gpioFromStorage.toInt();
+  }
+  if (gpioToUse == 0) {
+    if (!additionalParam.isEmpty()) {
+      gpioToUse = additionalParam.toInt();
+    }
+  }
+  switch (gpioToUse) {
+    case 5: gpioInUse = 5; break;
+    case 3: gpioInUse = 3; break;
+    case 16: gpioInUse = 16; break;
+    default: gpioInUse = 2; break;
+  }
+  Serial.print(F("GPIO IN USE="));
+  Serial.println(gpioInUse);
+
+
+  // Color mode from configuration storage
+  String colorModeFromStorage = bootstrapManager.readValueFromFile(ledManager.COLOR_MODE_FILENAME, ledManager.COLOR_MODE_PARAM);
+  if (!colorModeFromStorage.isEmpty() && colorModeFromStorage != ERROR && colorModeFromStorage.toInt() != 0) {
+    colorMode = colorModeFromStorage.toInt();
+  }
+  Serial.print(F("COLOR_MODE IN USE="));
+  Serial.println(colorMode);
+  ledManager.initLeds();
+
+#if defined(ESP8266)
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, LOW);
+#elif defined(ESP32)
+  pinMode(RELAY_PIN_DIG, OUTPUT);
+  digitalWrite(RELAY_PIN_DIG, LOW);
+  pinMode(RELAY_PIN_PICO, OUTPUT);
+  digitalWrite(RELAY_PIN_PICO, LOW);
+#endif
+
+#if defined(ESP32)
+  esp_task_wdt_init(3000, true); //enable panic so ESP32 restarts
+  esp_task_wdt_add(NULL); //add current thread to WDT watch
+#endif
+#ifdef TARGET_GLOWWORMLUCIFERINFULL
+  // Begin listening to UDP port
+  networkManager.UDP.begin(UDP_PORT);
+  networkManager.broadcastUDP.begin(UDP_BROADCAST_PORT);
+  Serial.print("Listening on UDP port ");
+  Serial.println(UDP_PORT);
+  networkManager.fpsData.reserve(200);
+  networkManager.prefsData.reserve(200);
+  networkManager.listenOnHttpGet();
+#if defined(ESP8266)
+  // Hey gateway, GlowWorm is here
+  delay(DELAY_500);
+  pingESP.ping(WiFi.gatewayIP());
+#endif
+#endif
 
 }
 
