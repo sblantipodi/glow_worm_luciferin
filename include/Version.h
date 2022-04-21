d1d8dfe41e4960e4e6122c8d41aa5da9c975f5e9
Davide Perini <perini.davide@dpsoftware.org>
2022-04-21 18:27:07 +0200

    GitHub Action auto update json file

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.5.9"
+        #define VERSION "5.5.10"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-04-21 17:56:03.649540"
+        #define BUILD_TIMESTAMP "2022-04-21 18:26:44.319321"
     #endif
     
