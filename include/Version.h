86dd27811435f99957dc87f2ecd677366b4fd977
Davide Perini <perini.davide@dpsoftware.org>
2022-09-03 16:33:24 +0200

    Light Dependant Resistor (LDR), first implementation.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.7.2"
+        #define VERSION "5.7.15"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-08-28 16:25:41.217665"
+        #define BUILD_TIMESTAMP "2022-08-31 00:55:47.981707"
     #endif
     
