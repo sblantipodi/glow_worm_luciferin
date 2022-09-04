1168e33aba004e2b46af191ec59d632d7c2c4a28
Davide Perini <perini.davide@dpsoftware.org>
2022-09-04 15:16:59 +0200

    ESP Web Tools improv

--- src/NetworkManager.cpp
+++ src/NetworkManager.cpp
@@ -1080,7 +1080,8 @@ void NetworkManager::checkConnection() {
     }
     framerate = framerateCounter > 0 ? framerateCounter / 10 : 0;
     framerateCounter = 0;
-    NetworkManager::sendStatus();
+    // TODO
+//    NetworkManager::sendStatus();
   }
 #elif  TARGET_GLOWWORMLUCIFERINLIGHT
   EVERY_N_SECONDS(15) {
