970e4b5c4fc158712dd83d0877aecadc3024a35a
Davide Perini <112591792+raybantracing@users.noreply.github.com>
2022-12-28 23:23:25 +0100

    libs update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.8.19"
+        #define VERSION "5.8.25"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-28 17:43:12.155335"
+        #define BUILD_TIMESTAMP "2022-12-28 23:15:44.753895"
     #endif
     
