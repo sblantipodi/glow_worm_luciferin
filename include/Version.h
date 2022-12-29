8fdf6179afef7784b868ea3a70235a33d5524de9
Davide Perini <112591792+raybantracing@users.noreply.github.com>
2022-12-29 20:45:00 +0100

    Node.js 12 actions are deprecated. Upgrade GitHub Actions to use Node.js 16.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.8.28"
+        #define VERSION "5.8.30"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-29 17:22:31.603446"
+        #define BUILD_TIMESTAMP "2022-12-29 20:41:51.104073"
     #endif
     
