3bed7e1f7856ab4f9b5be63e7b54bd429b8a59f4
Davide Perini <perini.davide@dpsoftware.org>
2022-12-30 01:40:15 +0100

    libs update

--- src/NetworkManager.cpp
+++ src/NetworkManager.cpp
@@ -127,11 +127,12 @@ void NetworkManager::manageDisconnections() {
  */
 void NetworkManager::manageQueueSubscription() {
 
+  //TODO
   BootstrapManager::subscribe(networkManager.lightSetTopic.c_str());
   BootstrapManager::subscribe(networkManager.streamTopic.c_str(), 0);
-  BootstrapManager::subscribe(networkManager.cmndReboot.c_str());
-  BootstrapManager::subscribe(networkManager.updateStateTopic.c_str());
-  BootstrapManager::subscribe(networkManager.unsubscribeTopic.c_str());
+//  BootstrapManager::subscribe(networkManager.cmndReboot.c_str());
+//  BootstrapManager::subscribe(networkManager.updateStateTopic.c_str());
+//  BootstrapManager::subscribe(networkManager.unsubscribeTopic.c_str());
   BootstrapManager::subscribe(networkManager.firmwareConfigTopic.c_str());
   BootstrapManager::subscribe(networkManager.ldrTopic.c_str());
 
