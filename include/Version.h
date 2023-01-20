78017cb56a18d18c2ae5ab87d12a64d76d1faaf2
Davide Perini <perini.davide@dpsoftware.org>
2023-01-20 12:25:10 +0100

    Arduino ESP82566 Core v3.1.1

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.8.98"
+        #define VERSION "5.8.99"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-01-20 12:08:25.197401"
+        #define BUILD_TIMESTAMP "2023-01-20 12:19:31.476272"
     #endif
     
