3ce7d1f25e2f8b06489eb20c09d5b8963d28c846
Davide Perini <perini.davide@dpsoftware.org>
2023-02-15 16:51:29 +0100

    web installer update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.8.4"
+        #define VERSION "5.8.5"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-02-15 16:22:03.159838"
+        #define BUILD_TIMESTAMP "2023-02-15 16:50:30.774012"
     #endif
     
