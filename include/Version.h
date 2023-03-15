1904500f18f756407400a01d600c28bd5db782d5
Davide Perini <perini.davide@dpsoftware.org>
2023-03-15 21:22:19 +0100

    libs update

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.9.8"
+        #define VERSION "5.9.9"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-03-15 19:58:09.493161"
+        #define BUILD_TIMESTAMP "2023-03-15 20:01:53.549451"
     #endif
     
