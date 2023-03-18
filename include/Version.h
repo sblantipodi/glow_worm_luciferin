9878893a163b07d5ffec8abc12a59f42626cf280
Davide Perini <perini.davide@dpsoftware.org>
2023-03-18 14:10:14 +0100

    RGBW SK6812 performance boost. +30% FPS and reduced latency.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.9.10"
+        #define VERSION "5.9.12"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-03-16 22:33:41.212296"
+        #define BUILD_TIMESTAMP "2023-03-17 21:03:19.403433"
     #endif
     
