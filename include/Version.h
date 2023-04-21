c2a87bcb9813a200646d32d75ac3cfcae1f5fac7
Davide Perini <perini.davide@dpsoftware.org>
2023-04-22 00:11:05 +0200

    esp web tools quircks

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.9.38"
+        #define VERSION "5.9.44"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-04-13 21:00:01.053747"
+        #define BUILD_TIMESTAMP "2023-04-21 20:17:06.237734"
     #endif
     
