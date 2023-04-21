fa92b22f0162f389364b145250f65706a167c1db
Davide Perini <perini.davide@dpsoftware.org>
2023-04-21 16:26:44 +0200

    esp web tools quircks

--- src/GlowWormLuciferin.cpp
+++ src/GlowWormLuciferin.cpp
@@ -60,31 +60,13 @@ void setup() {
 
 #ifdef TARGET_GLOWWORMLUCIFERINLIGHT
   MAC = WiFi.macAddress();
-  bootstrapManager.littleFsInit();
-  configureLeds();
+#if defined(ESP8266)
+  if (!LittleFS.begin()) {
+#elif defined(ESP32)
+  if (!LittleFS.begin(true)) {
 #endif
-
-#ifdef TARGET_GLOWWORMLUCIFERINFULL
-  String ap = bootstrapManager.readValueFromFile(AP_FILENAME, AP_PARAM);
-  if (!ap.isEmpty() && ap != ERROR && ap.toInt() == 10) {
-    setApState(11);
-    ledManager.setColor(0, 255, 0);
-  } else if (!ap.isEmpty() && ap != ERROR && ap.toInt() == 11) {
-    setApState(12);
-    ledManager.setColor(0, 0, 255);
-  } else if (!ap.isEmpty() && ap != ERROR && ap.toInt() == 12) {
-    // TODO decomment
-//    bootstrapManager.littleFsInit();
-    bootstrapManager.isWifiConfigured();
-    setApState(13);
-    ledManager.setColor(255, 75, 0);
-    // TODO decomment
-
-//    bootstrapManager.launchWebServerCustom(false, manageApRoot);
-  } else if (!ap.isEmpty() && ap != ERROR && ap.toInt() == 13) {
-    setApState(0);
-  } else {
-    configureLeds();
+    Serial.println("LittleFS mount failed");
+    return;
   }
 #endif
 
@@ -99,14 +81,37 @@ void setup() {
   Serial.println(networkManager.topicInUse);
 
   // Bootsrap setup() with Wifi and MQTT functions
-
-  // TODO decomment right
-  bootstrapManager.bootstrapSetup(NetworkManager::manageDisconnections, NetworkManager::manageHardwareButton,
-                                  NetworkManager::callback, true, manageApRoot);
-
-//  bootstrapManager.bootstrapSetup(NetworkManager::manageDisconnections, NetworkManager::manageHardwareButton, NetworkManager::callback);
-
+  bootstrapManager.bootstrapSetup(NetworkManager::manageDisconnections, NetworkManager::manageHardwareButton, NetworkManager::callback);
 #endif
+
+  // GPIO pin from configuration storage, overwrite the one saved during initial Arduino Bootstrapper config
+  String gpioFromStorage = bootstrapManager.readValueFromFile(GPIO_FILENAME, GPIO_PARAM);
+  int gpioToUse = 0;
+  if (!gpioFromStorage.isEmpty() && gpioFromStorage != ERROR && gpioFromStorage.toInt() != 0) {
+    gpioToUse = gpioFromStorage.toInt();
+  }
+  if (gpioToUse == 0) {
+    if (!additionalParam.isEmpty()) {
+      gpioToUse = additionalParam.toInt();
+    }
+  }
+  switch (gpioToUse) {
+    case 5: gpioInUse = 5; break;
+    case 3: gpioInUse = 3; break;
+    case 16: gpioInUse = 16; break;
+    default: gpioInUse = 2; break;
+  }
+  Serial.print(F("GPIO IN USE="));
+  Serial.println(gpioInUse);
+
+  // Color mode from configuration storage
+  String colorModeFromStorage = bootstrapManager.readValueFromFile(ledManager.COLOR_MODE_FILENAME, ledManager.COLOR_MODE_PARAM);
+  if (!colorModeFromStorage.isEmpty() && colorModeFromStorage != ERROR && colorModeFromStorage.toInt() != 0) {
+    colorMode = colorModeFromStorage.toInt();
+  }
+  Serial.print(F("COLOR_MODE IN USE="));
+  Serial.println(colorMode);
+  ledManager.initLeds();
 
   // Color mode from configuration storage
   String ldrFromStorage = bootstrapManager.readValueFromFile(ledManager.LDR_FILENAME, ledManager.LDR_PARAM);
