9600d12e4d7d2b30dfbb9e9f92651187a85ca550
Davide Perini <perini.davide@dpsoftware.org>
2023-05-13 12:48:59 +0000

    CI release for firmware web installer

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.9.48"
+        #define VERSION "5.10.6"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-05-08 12:36:18.456875"
+        #define BUILD_TIMESTAMP "2023-05-13 12:46:03.685494"
     #endif
     
