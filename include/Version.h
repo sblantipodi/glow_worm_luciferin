6c28593fd4f2c8b716a4bbd0e9da0451e068c82c
Davide Perini <perini.davide@dpsoftware.org>
2023-05-15 10:32:31 +0200

    reduced footprint, FastLED lib ditched.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.10.6"
+        #define VERSION "5.10.7"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-05-14 21:45:15.663417"
+        #define BUILD_TIMESTAMP "2023-05-15 09:17:08.482450"
     #endif
     
