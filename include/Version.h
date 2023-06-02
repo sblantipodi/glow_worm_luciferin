dd7a2d18b5890251459ef72349c77ff1ccb4d287
Davide Perini <perini.davide@dpsoftware.org>
2023-06-02 14:25:30 +0200

    initial support for ESP32-C3, ESP32-S2, ESP32-S3 (not yet stable)

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.10.38"
+        #define VERSION "5.10.41"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-05-30 13:53:31.971704"
+        #define BUILD_TIMESTAMP "2023-06-02 14:21:07.639952"
     #endif
     
