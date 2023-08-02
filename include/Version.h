e5bb17f43de8f65fbfd7694642b5c2d5873778c7
Davide Perini <perini.davide@dpsoftware.org>
2023-08-02 16:28:11 +0200

    Glow Worm Luciferin release (v5.11.8)

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.10.8"
+        #define VERSION "5.10.6"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-07-29 02:21:29.104751"
+        #define BUILD_TIMESTAMP "2023-08-02 16:24:09.913906"
     #endif
     
