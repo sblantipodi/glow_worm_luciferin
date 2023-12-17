f54c3020b682498e8d353e7844fd44e24b1839e5
Davide Perini <perini.davide@dpsoftware.org>
2023-12-17 15:51:46 +0100

    merge

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.12.11"
+        #define VERSION "5.12.40"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2023-12-09 12:52:49.052243"
