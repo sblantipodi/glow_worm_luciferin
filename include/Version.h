324b0c38a05552a856e086a85fc7d0444c54bf61
Davide Perini <perini.davide@dpsoftware.org>
2024-04-12 12:37:45 +0200

    espressif32@6.6.0

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.13.17"
+        #define VERSION "5.13.18"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-01-26 15:14:43.137575"
+        #define BUILD_TIMESTAMP "2024-04-12 15:14:43.137575"
     #endif
     
