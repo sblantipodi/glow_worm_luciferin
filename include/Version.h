ba2e5b49dcc5cdc13e6b8227072c65e51acb1927
sblantipodi <perini.davide@dpsoftware.org>
2024-05-14 18:22:16 +0200

    espressif32@6.7.0

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.13.19"
+        #define VERSION "5.13.20"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-04-12 15:14:43.137575"
