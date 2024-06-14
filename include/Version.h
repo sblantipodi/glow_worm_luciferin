cb78e751e80620ec9e908c44f38386ec1bc723e2
Davide Perini <perini.davide@dpsoftware.org>
2024-06-14 16:01:15 +0200

    github action update for parallel build

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.13.141"
+        #define VERSION "5.13.142"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-06-13 11:06:41.760413"
