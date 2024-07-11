815f94d59152240893f50bce36bfdf18a23d4082
Davide Perini <perini.davide@dpsoftware.org>
2024-07-11 15:10:53 +0200

    usb serial

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.14.39"
+        #define VERSION "5.14.40"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-07-11 10:18:53.667355"
