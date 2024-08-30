fcb5ecbdc6d67e389558c9d0a23fcd55a6a7ee39
Davide Perini <perini.davide@dpsoftware.org>
2024-08-30 15:19:06 +0200

    Built-in led management

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.15.17"
+        #define VERSION "5.16.10"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-08-17 12:56:11.203608"
+        #define BUILD_TIMESTAMP "2024-08-30 15:18:09.099370"
     #endif
     
