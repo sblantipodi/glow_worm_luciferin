79d0e86a880823cc0abbc433d752e2e33a9332b4
Davide Perini <perini.davide@dpsoftware.org>
2024-08-31 01:18:24 +0200

    IDF5

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.15.4"
+        #define VERSION "5.12.25"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-08-02 13:34:16.324757"
+        #define BUILD_TIMESTAMP "2024-08-31 01:15:12.953231"
     #endif
     
