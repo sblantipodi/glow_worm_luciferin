0e982613fb8f0826aa7390839e42fefc8281dc56
Davide Perini <perini.davide@dpsoftware.org>
2024-09-10 11:07:27 +0200

    NeoPixelBus@2.8.2 + updated serial

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.16.29"
+        #define VERSION "5.16.30"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-09-07 17:11:31.698757"
+        #define BUILD_TIMESTAMP "2024-09-10 11:03:11.308266"
     #endif
     
