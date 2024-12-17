81411dad2512b0d4c1bcf632824c909a43bd7aed
Davide Perini <perini.davide@dpsoftware.org>
2024-12-17 16:24:21 +0100

    new effects

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.16.152"
+        #define VERSION "5.16.7"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-12-17 14:38:58.822045"
