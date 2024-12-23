e71fc3b980db3873fbae01ca36d96766e56b8d6c
Davide Perini <perini.davide@dpsoftware.org>
2024-12-23 18:21:07 +0100

    esp32 ping

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.17.11"
+        #define VERSION "5.17.0"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2024-12-19 12:38:01.175435"
