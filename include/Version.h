016e26cc534cc40e61c2b2313c99ced18532960f
Davide Perini <perini.davide@dpsoftware.org>
2025-01-03 12:59:00 +0100

    faster reconnection

--- include/Version.h
+++ include/Version.h
@@ -4,6 +4,6 @@
         #define VERSION "5.17.9"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-12-29 16:37:49.914809"
+        #define BUILD_TIMESTAMP "2025-01-02 14:18:51.891470"
     #endif
     
