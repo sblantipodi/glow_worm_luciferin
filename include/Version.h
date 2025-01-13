dd92fa0564def7cd9a9feeece7334b44376bb0f4
Davide Perini <perini.davide@dpsoftware.org>
2025-01-13 21:38:09 +0100

    web interface fix

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.18.2"
+        #define VERSION "5.18.28"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-01-04 23:29:08.825944"
+        #define BUILD_TIMESTAMP "2025-01-13 21:34:34.082868"
     #endif
     
