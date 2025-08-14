d01327dffc7796d676d8c9e33a09541b096534d9
Davide Perini <perini.davide@dpsoftware.org>
2025-08-14 12:56:33 +0200

    invert relay option

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.21.51"
+        #define VERSION "5.21.3"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2025-08-09 14:31:39.066602"
