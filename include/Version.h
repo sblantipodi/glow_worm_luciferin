4c72075d3177aef531d0d086f72c1263d6e5aa73
Davide Perini <perini.davide@dpsoftware.org>
2025-08-17 15:08:14 +0200

    update

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.22.4"
+        #define VERSION "5.22.5"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2025-08-14 18:59:59.535524"
