1ab16b9043b3d76d599d1739f12b34bb3f1c4fd0
Davide Perini <perini.davide@dpsoftware.org>
2025-09-28 14:54:16 +0200

    Allow the use of the Improv protocol even without internet or MQTT connectivity.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.22.14"
+        #define VERSION "5.22.23"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-09-27 23:43:45.069810"
+        #define BUILD_TIMESTAMP "2025-09-28 14:51:16.938030"
     #endif
     
