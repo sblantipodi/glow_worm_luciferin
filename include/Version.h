a436c75e737a337cf7505916be4ef7464a332f68
Davide Perini <perini.davide@dpsoftware.org>
2025-11-13 16:40:25 +0100

    beta build

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.22.44"
+        #define VERSION "5.23.1"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-10-13 14:30:11.650654"
+        #define BUILD_TIMESTAMP "2025-11-13 12:45:36.101379"
     #endif
     
