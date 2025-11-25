4f87b6ec5c94da9fe453cbd11aad1f306cc46cd8
Davide Perini <perini.davide@dpsoftware.org>
2025-11-25 09:20:54 +0100

    LDR readings on a separate tasks

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.1"
+        #define VERSION "5.22.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-11-13 12:45:36.101379"
+        #define BUILD_TIMESTAMP "2025-11-24 22:28:58.024301"
     #endif
     
