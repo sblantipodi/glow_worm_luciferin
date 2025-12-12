bce5df6045e880b86d769d8cddb521c9b4a5fd1a
Davide Perini <perini.davide@dpsoftware.org>
2025-12-12 15:04:42 +0100

    core 3

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -270,46 +270,46 @@ void Globals::sendSerialInfo() {
     if (currentMillisSendSerial > lastUdpMsgReceived + DELAY_1000) {
       framerateSerial = framerateCounterSerial > 0 ? framerateCounterSerial / 10 : 0;
       framerateCounterSerial = 0;
-        Serial.printf("framerate:%f\n", (framerateSerial > 0.5 ? framerateSerial : 0));
+        Serial.printf("framerate:%f\r\n", (framerateSerial > 0.5 ? framerateSerial : 0));
 #ifdef TARGET_GLOWWORMLUCIFERINLIGHT
-        Serial.printf("firmware:%s\n", "LIGHT");
+        Serial.printf("firmware:%s\r\n", "LIGHT");
 #else
-        Serial.printf("firmware:%s\n", "FULL");
-        Serial.printf("mqttopic:%s\n", netManager.topicInUse.c_str());
+        Serial.printf("firmware:%s\r\n", "FULL");
+        Serial.printf("mqttopic:%s\r\n", netManager.topicInUse.c_str());
 #endif
-        Serial.printf("ver:%s\n", VERSION);
-        Serial.printf("lednum:%d\n", ledManager.dynamicLedNum);
+        Serial.printf("ver:%s\r\n", VERSION);
+        Serial.printf("lednum:%d\r\n", ledManager.dynamicLedNum);
 #if defined(ESP8266)
-        Serial.printf("board:%s\n", "ESP8266");
+        Serial.printf("board:%s\r\n", "ESP8266");
 #endif
 #if CONFIG_IDF_TARGET_ESP32C3
-        Serial.printf("board:%s\n", "ESP32_C3_CDC");
+        Serial.printf("board:%s\r\n", "ESP32_C3_CDC");
 #elif CONFIG_IDF_TARGET_ESP32S2
-        Serial.printf("board:%s\n", "ESP32_S2");
+        Serial.printf("board:%s\r\n", "ESP32_S2");
 #elif CONFIG_IDF_TARGET_ESP32S3
 #if ARDUINO_USB_MODE==1
-        Serial.printf("board:%s\n", "ESP32_S3");
+        Serial.printf("board:%s\r\n", "ESP32_S3");
 #else
-        Serial.printf("board:%s\n", "ESP32_S3");
+        Serial.printf("board:%s\r\n", "ESP32_S3");
 #endif
 #elif CONFIG_IDF_TARGET_ESP32
-        Serial.printf("board:%s\n", "ESP32");
+        Serial.printf("board:%s\r\n", "ESP32");
 #endif
-        Serial.printf("MAC:%s\n", MAC.c_str());
-        Serial.printf("gpio:%d\n", gpioInUse);
-        Serial.printf("gpioClock:%d\n", gpioClockInUse);
-        Serial.printf("baudrate:%d\n", baudRateInUse);
-        Serial.printf("effect:%d\n", Globals::effectToInt(effect));
-        Serial.printf("colorMode:%d\n", colorMode);
-        Serial.printf("colorOrder:%d\n", colorOrder);
-        Serial.printf("white:%d\n", whiteTempInUse);
+        Serial.printf("MAC:%s\r\n", MAC.c_str());
+        Serial.printf("gpio:%d\r\n", gpioInUse);
+        Serial.printf("gpioClock:%d\r\n", gpioClockInUse);
+        Serial.printf("baudrate:%d\r\n", baudRateInUse);
+        Serial.printf("effect:%d\r\n", Globals::effectToInt(effect));
+        Serial.printf("colorMode:%d\r\n", colorMode);
+        Serial.printf("colorOrder:%d\r\n", colorOrder);
+        Serial.printf("white:%d\r\n", whiteTempInUse);
         if (ldrEnabled) {
-          Serial.printf("ldr:%d\n", ((ldrValue * 100) / ldrDivider));
+          Serial.printf("ldr:%d\r\n", ((ldrValue * 100) / ldrDivider));
       }
-      Serial.printf("relayPin:%d\n", relayPin);
-      Serial.printf("relInv:%d\n", relInv);
-      Serial.printf("sbPin:%d\n", sbPin);
-      Serial.printf("ldrPin:%d\n", ldrPin);
+      Serial.printf("relayPin:%d\r\n", relayPin);
+      Serial.printf("relInv:%d\r\n", relInv);
+      Serial.printf("sbPin:%d\r\n", sbPin);
+      Serial.printf("ldrPin:%d\r\n", ldrPin);
       Serial.flush();
     }
   }
