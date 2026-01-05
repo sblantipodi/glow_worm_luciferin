c58d09b91ce8113c6529a91af8df7dc5bbd3941e
Davide Perini <perini.davide@dpsoftware.org>
2026-01-05 18:45:39 +0100

    WiFi/Ethernet coexistence

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.175"
+        #define VERSION "5.23.199"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-01-05 14:58:30.236782"
+        #define BUILD_TIMESTAMP "2026-01-05 18:32:19.439648"
     #endif
     
