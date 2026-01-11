234435c7ed7f60e9562a4748455b4bf72dfd233c
Davide Perini <perini.davide@dpsoftware.org>
2026-01-11 02:31:38 +0100

    antipattern fix

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.224"
+        #define VERSION "5.23.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-01-08 13:14:28.963487"
+        #define BUILD_TIMESTAMP "2026-01-09 22:26:52.083535"
     #endif
     
