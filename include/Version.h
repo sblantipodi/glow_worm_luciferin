389202679f00f2f219c8ca2ac71f080173776571
Davide Perini <perini.davide@dpsoftware.org>
2026-02-03 18:32:42 +0100

    DPsofware custom improv protocol

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.226"
+        #define VERSION "5.23.262"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-01-26 16:59:52.721887"
+        #define BUILD_TIMESTAMP "2026-02-03 15:28:40.367222"
     #endif
     
