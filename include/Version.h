b32f915f4b3af0ed4c311ad9667e54ea3ca6b130
Davide Perini <perini.davide@dpsoftware.org>
2026-04-27 23:36:55 +0200

    actions/upload-artifact@v7.0.1

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.134"
+        #define VERSION "5.23.136"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-04-27 09:42:12.963780"
+        #define BUILD_TIMESTAMP "2026-04-27 23:36:08.280116"
     #endif
     
