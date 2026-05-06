9dfebc8d3533d6454b2be9ffb785e84dbcc98da0
Davide Perini <perini.davide@dpsoftware.org>
2026-05-06 22:38:50 +0200

    rebuild

--- include/Version.h
+++ include/Version.h
@@ -1,7 +1,7 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.23.139"
+        #define VERSION "5.23.140"
     #endif
     #ifndef BUILD_TIMESTAMP
         #define BUILD_TIMESTAMP "2026-05-02 22:28:14.754182"
