cb4b80a8dcd8daaa98f2738e1112e3a3c8292de5
Davide Perini <perini.davide@dpsoftware.org>
2026-06-08 13:09:03 +0200

    "RLE (Run-Length Encoding) group map from the LED matrix"

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "5.25.108"
+        #define VERSION "5.25.182"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-06-06 01:27:12.494452"
+        #define BUILD_TIMESTAMP "2026-06-08 11:28:55.534348"
     #endif
     
