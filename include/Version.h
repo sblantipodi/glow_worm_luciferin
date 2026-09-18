a4e0f8d439df07208edc99b3c9d7fb164286ea87
Davide Perini <perini.davide@dpsoftware.org>
2026-09-18 15:20:25 +0200

    max width css

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "6.0.0"
+        #define VERSION "6.0.3"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-08-23 17:17:07.913481"
+        #define BUILD_TIMESTAMP "2026-09-18 15:17:07.552790"
     #endif
     
