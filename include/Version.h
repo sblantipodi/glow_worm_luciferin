4095ffd6da2d70510da4310582d941a8ce61b0e3
Davide Perini <perini.davide@dpsoftware.org>
2026-09-23 22:55:18 +0200

    Make W5500 polling period configurable by @sblantipodi in #12856.
    Espressif32 -> Arduino v4.0.0-RC1 based on ESP-IDF v6.1

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "6.0.3"
+        #define VERSION "6.0.7"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-09-18 15:17:07.552790"
+        #define BUILD_TIMESTAMP "2026-09-23 22:24:27.793517"
     #endif
     
